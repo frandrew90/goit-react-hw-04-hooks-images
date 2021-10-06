034c1adb807f18a0f07ea4e7570cf610a557f0d5
frandrew90 <frandrewjs@gmail.com>
2021-10-06 20:32:26 +0300

    made hw4

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Searchbar from './searchbar/Searchbar';
 import { getPhoto } from '../services/ApiServices';
 import ImageGallery from './imageGallery/ImageGallery';
@@ -6,98 +6,205 @@ import Modal from './modal/Modal';
 import Button from './button/Button';
 import Loader from './loader/Loader';
 
-class App extends Component {
-  state = {
-    find: '',
-    gallery: [],
-    page: 1,
-    total: 0,
-    largeImageURL: {},
-    showModal: false,
-    isLoading: false,
-  };
+const initialState = {
+  find: '',
+  gallery: [],
+  page: 1,
+  total: 0,
+  largeImageURL: {},
+  showModal: false,
+  isLoading: false,
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.find !== this.state.find) {
-      this.setState({
-        gallery: [],
-      });
-      this.makeGallery();
+const App = () => {
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    if (!state.find) {
       return;
     }
-  }
 
-  makeGallery = () => {
-    const { find, page } = this.state;
-    this.setState({ isLoading: true });
+    setState(prevState => ({
+      ...prevState,
+      gallery: [],
+    }));
+    makeGallery();
+
+    // eslint-disable-next-line
+  }, [state.find]);
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.find !== state.find) {
+  //     setState({
+  //       gallery: [],
+  //     });
+  //     makeGallery();
+  //     return;
+  //   }
+  // }
+
+  const makeGallery = () => {
+    const { find, page } = state;
+    setState(prevState => ({ ...prevState, isLoading: true }));
 
     getPhoto(find, page)
       .then(({ hits, total }) => {
         // console.log('hit', hits);
-        this.setState(prevState => ({
+        setState(prevState => ({
+          ...prevState,
           gallery: [...prevState.gallery, ...hits],
           page: prevState.page + 1,
           total,
         }));
         if (page !== 1) {
-          this.scroll();
+          scroll();
         }
-        // console.log('state', this.state.gallery);
+        // console.log('state', state.gallery);
         if (total === 0) {
           alert('There are no pictures');
         }
       })
       .catch(error => alert(error.message))
-      .finally(() => this.setState({ isLoading: false }));
+      .finally(() =>
+        setState(prevState => ({ ...prevState, isLoading: false })),
+      );
   };
 
-  onFormSubmit = find => {
-    this.setState({ find, page: 1 });
+  const onFormSubmit = find => {
+    setState(prevState => ({ ...prevState, find, page: 1 }));
   };
 
-  scroll = () => {
+  const scroll = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
   };
 
-  toggleModal = () => {
-    this.setState(prev => ({
-      showModal: !prev.showModal,
-    }));
+  const toggleModal = () => {
+    setState(prev => ({ ...prev, showModal: !prev.showModal }));
   };
 
-  showBtnLoadMore = () => {
-    return Math.ceil(this.state.total / 12) !== this.state.page - 1;
+  const showBtnLoadMore = () => {
+    return Math.ceil(state.total / 12) !== state.page - 1;
   };
 
-  onPictureOpen = largeImage => {
-    this.setState({ largeImageURL: largeImage });
-    this.toggleModal();
+  const onPictureOpen = largeImage => {
+    setState(prevState => ({ ...prevState, largeImageURL: largeImage }));
+    toggleModal();
   };
 
-  render() {
-    const { gallery, showModal, largeImageURL, isLoading, total } = this.state;
+  const { gallery, showModal, largeImageURL, isLoading, total } = state;
 
-    return (
-      <>
-        <Searchbar onSubmit={this.onFormSubmit} />
+  return (
+    <>
+      <Searchbar onSubmit={onFormSubmit} />
 
-        {/* {console.log('render', gallery)} */}
-        {gallery.length !== 0 && (
-          <ImageGallery gallery={gallery} onPictureOpen={this.onPictureOpen} />
-        )}
-        {showModal && (
-          <Modal onClose={this.toggleModal} largeImage={largeImageURL} />
-        )}
-        {isLoading && <Loader />}
-        {this.showBtnLoadMore() && total !== 0 && (
-          <Button getPhoto={this.makeGallery} />
-        )}
-      </>
-    );
-  }
-}
+      {/* {console.log('render', gallery)} */}
+      {gallery.length !== 0 && (
+        <ImageGallery gallery={gallery} onPictureOpen={onPictureOpen} />
+      )}
+      {showModal && <Modal onClose={toggleModal} largeImage={largeImageURL} />}
+      {isLoading && <Loader />}
+      {showBtnLoadMore() && total !== 0 && <Button getPhoto={makeGallery} />}
+    </>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   state = {
+//     find: '',
+//     gallery: [],
+//     page: 1,
+//     total: 0,
+//     largeImageURL: {},
+//     showModal: false,
+//     isLoading: false,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.find !== state.find) {
+//       setState({
+//         gallery: [],
+//       });
+//       makeGallery();
+//       return;
+//     }
+//   }
+
+//   makeGallery = () => {
+//     const { find, page } = state;
+//     setState({ isLoading: true });
+
+//     getPhoto(find, page)
+//       .then(({ hits, total }) => {
+//         // console.log('hit', hits);
+//         setState(prevState => ({
+//           gallery: [...prevState.gallery, ...hits],
+//           page: prevState.page + 1,
+//           total,
+//         }));
+//         if (page !== 1) {
+//           scroll();
+//         }
+//         // console.log('state', state.gallery);
+//         if (total === 0) {
+//           alert('There are no pictures');
+//         }
+//       })
+//       .catch(error => alert(error.message))
+//       .finally(() => setState({ isLoading: false }));
+//   };
+
+//   onFormSubmit = find => {
+//     setState({ find, page: 1 });
+//   };
+
+//   scroll = () => {
+//     window.scrollTo({
+//       top: document.documentElement.scrollHeight,
+//       behavior: 'smooth',
+//     });
+//   };
+
+//   toggleModal = () => {
+//     setState(prev => ({
+//       showModal: !prev.showModal,
+//     }));
+//   };
+
+//   showBtnLoadMore = () => {
+//     return Math.ceil(state.total / 12) !== state.page - 1;
+//   };
+
+//   onPictureOpen = largeImage => {
+//     setState({ largeImageURL: largeImage });
+//     toggleModal();
+//   };
+
+//   render() {
+//     const { gallery, showModal, largeImageURL, isLoading, total } = state;
+
+//     return (
+//       <>
+//         <Searchbar onSubmit={onFormSubmit} />
+
+//         {/* {console.log('render', gallery)} */}
+//         {gallery.length !== 0 && (
+//           <ImageGallery gallery={gallery} onPictureOpen={onPictureOpen} />
+//         )}
+//         {showModal && (
+//           <Modal onClose={toggleModal} largeImage={largeImageURL} />
+//         )}
+//         {isLoading && <Loader />}
+//         {showBtnLoadMore() && total !== 0 && (
+//           <Button getPhoto={makeGallery} />
+//         )}
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
